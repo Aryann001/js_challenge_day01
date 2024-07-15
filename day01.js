377558657a7bbdb18b1c2566004a3a233578d28d
Aryan <aryanbaghel040@gmail.com>
2024-07-15 21:31:38 +0530

    first commit

---
+++ day01.js
@@ -0,0 +1,35 @@
+// Task 1
+var number = 23
+console.log(number)
+
+// Task 2
+let string = "Chai aur Code"
+console.log(string)
+
+// Task 3 
+const boolean = true
+console.log(boolean)
+
+// Task 4 
+let array = [1, 2, 3]
+let obj = {success: true}
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof array)
+console.log(typeof obj)
+
+// Task 5 
+let newVariable = 1
+console.log(newVariable)
+
+newVariable = 33
+console.log(newVariable)
+
+// Task 6 
+const newVariable2 = 1
+console.log(newVariable2)
+
+// newVariable2 = 33
+// console.log(newVariable2)
